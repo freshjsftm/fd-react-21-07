74e8c46ae5dba1c8a168f6de172f9a48f24d383a
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-07-22 16:51:20 +0300

    Prepare for task CiaoHeading

--- src/App.js
+++ src/App.js
@@ -1,87 +1,13 @@
-import React, { Component } from "react";
-import Ciao from "./components/Ciao";
+import React from "react";
 import "./App.css";
+import CiaoSection from "./components/CiaoSection";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      users: [
-        {
-          id: 4,
-          fname: "Elon",
-          lname: "Musk",
-        },
-        {
-          id: 1,
-          fname: "Molly",
-          lname: "Dolly",
-        },
-        {
-          id: 3,
-          fname: "Elen",
-          lname: "Musk",
-        },
-        {
-          id: 2,
-          fname: "Tom",
-          lname: "Chrom",
-        },
-        {
-          id: 5,
-          fname: "Fred",
-          lname: "Bredsk",
-        },
-      ],
-      isUpSortById:true,
-      isUpSortByLname:true
-    };
-  }
-  sortById=()=>{
-    const { users, isUpSortById } = this.state;
-    const copyUsers = JSON.parse(JSON.stringify(users));
-    copyUsers.sort((a, b) =>{
-      return isUpSortById ? a.id - b.id : b.id - a.id ;
-    })
-    this.setState({
-      users:copyUsers,
-      isUpSortById: !isUpSortById
-    })
-  }
-  sortByLname=()=>{
-    const { users, isUpSortByLname } = this.state;
-    const copyUsers = [...users];
-    copyUsers.sort((a, b) => {
-      if(a.lname > b.lname) {
-        return isUpSortByLname ? 1 : -1;
-      }
-      if(a.lname < b.lname) {
-        return isUpSortByLname ? -1 : 1;
-      }
-      return 0;
-    })
-    this.setState({
-      users:copyUsers,
-      isUpSortByLname: !isUpSortByLname
-    })
-  }
-  render() {
-    const { users, isUpSortById , isUpSortByLname} = this.state;
-    return (
-      <>
-        <h1 className="heading" title="react">Hi!</h1>
-        <button onClick={this.sortById}>sort by ID {isUpSortById?'DN':'UP'}</button>
-        <button onClick={this.sortByLname}>sort by last name {isUpSortByLname?'DN':'UP'}</button>
-        <ol>
-          {users.map((user) => 
-            <li key={user.id}>
-              <Ciao  fname={user.fname} sname={user.lname} id={user.id} />
-            </li>
-          )}
-        </ol>
-      </>
-    );
-  }
-}
+const App = () => {
+  return (
+    <>
+      <CiaoSection />
+    </>
+  );
+};
 
 export default App;
