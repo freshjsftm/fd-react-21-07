f4bc940188774c0c087104e83596e2d5b4841e54
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-07-22 16:16:13 +0300

    Add sort by alphabet

--- src/App.js
+++ src/App.js
@@ -33,12 +33,12 @@ class App extends Component {
           lname: "Bredsk",
         },
       ],
-      isUpSortById:true
+      isUpSortById:true,
+      isUpSortByLname:true
     };
   }
   sortById=()=>{
     const { users, isUpSortById } = this.state;
-    //const copyUsers = [...users];
     const copyUsers = JSON.parse(JSON.stringify(users));
     copyUsers.sort((a, b) =>{
       return isUpSortById ? a.id - b.id : b.id - a.id ;
@@ -48,12 +48,30 @@ class App extends Component {
       isUpSortById: !isUpSortById
     })
   }
+  sortByLname=()=>{
+    const { users, isUpSortByLname } = this.state;
+    const copyUsers = [...users];
+    copyUsers.sort((a, b) => {
+      if(a.lname > b.lname) {
+        return isUpSortByLname ? 1 : -1;
+      }
+      if(a.lname < b.lname) {
+        return isUpSortByLname ? -1 : 1;
+      }
+      return 0;
+    })
+    this.setState({
+      users:copyUsers,
+      isUpSortByLname: !isUpSortByLname
+    })
+  }
   render() {
-    const { users, isUpSortById } = this.state;
+    const { users, isUpSortById , isUpSortByLname} = this.state;
     return (
       <>
         <h1 className="heading" title="react">Hi!</h1>
         <button onClick={this.sortById}>sort by ID {isUpSortById?'DN':'UP'}</button>
+        <button onClick={this.sortByLname}>sort by last name {isUpSortByLname?'DN':'UP'}</button>
         <ol>
           {users.map((user) => 
             <li key={user.id}>
