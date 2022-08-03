d4c351e5985805f6adc45ade13b5b34964b7ad9c
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-08-03 15:01:15 +0300

    Refactor getRandomUsers

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,28 @@
+import queryString from "query-string";
+import config from "../configs";
+/**
+ *
+ * @param {object} options
+ * @param {number} options.page
+ * @param {number} options.results
+ * @param {string} options.seed
+ * @param {string} options.nat
+ * @param {string} options.gender
+ * @param {string} options.inc
+ * @returns
+ */
 export const getRandomUsers = (options = {}) => {
-  const {page, results=5} = options;
-  return fetch(
-    `https://randomuser.me/api/?page=${page}&results=${results}&seed=fd20221&nat=gb`
-  ).then((response) => response.json());
+  const defaultOption = {
+    page: 1,
+    results: config.DEFAULT_AMOUNT,
+    seed: config.API_KEY,
+    nat: "gb",
+    gender: "male",
+    inc:config.DEFAULT_USER_PROP,
+  };
+  const finallyOptions = { ...defaultOption, ...options };
+  const queryOptions = queryString.stringify(finallyOptions);
+  return fetch(`${config.API_BASE_URL}?${queryOptions}`).then((response) =>
+    response.json()
+  );
 };
---
+++ src/configs/index.js
@@ -0,0 +1,8 @@
+const config = {
+  API_KEY: 'fd20221',
+  API_BASE_URL: 'https://randomuser.me/api/',
+  DEFAULT_AMOUNT :5, 
+  DEFAULT_USER_PROP: "gender,name,nat,email,login"
+}
+
+export default config;
