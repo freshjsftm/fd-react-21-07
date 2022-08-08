11ac4a89270dc8258e701755dc3110a81b27f404
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-08-08 17:06:40 +0300

    Add theme context to Tree

--- src/components/Tree/index.js
+++ src/components/Tree/index.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import Parent from './Parent';
+import React from "react";
+import cx from "classnames";
+import Parent from "./Parent";
+import { ThemeContext } from "../../contexts";
+import CONSTANTS from "../../constants";
+import styles from "./Tree.module.scss";
+const { THEMES } = CONSTANTS;
 
 const Tree = (props) => {
-
   return (
-    <div>
-      <h2>Tree</h2>
-      <Parent/>
-    </div>
+    <ThemeContext.Consumer>
+      {([theme]) => {
+        const stylesContainer = cx(styles.container, {
+          [styles.light]: theme === THEMES.LIGHT,
+          [styles.dark]: theme === THEMES.DARK,
+        });
+        return (
+          <div className={stylesContainer}>
+            <h2>Tree</h2>
+            <Parent />
+          </div>
+        );
+      }}
+    </ThemeContext.Consumer>
   );
-}
+};
 
 export default Tree;
