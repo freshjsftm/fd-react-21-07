92ff119c8ed94bba4dde9a0f0d22c628fc101e4f
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-08-15 15:24:05 +0300

    Add state as object

--- src/components/LearnHooks/index.js
+++ src/components/LearnHooks/index.js
@@ -1,22 +1,32 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
 const LearnHooks = () => {
-  const [count, setCount] = useState(0);
-  const [step, setStep] = useState(10);
+  const [state, setState] = useState({ x: 0, y: 0, count: 0 });
+  //console.log('render')
+  const handlerMove = (event) => {
+    setState({
+      ...state,
+      x: event.clientX,
+      y: event.clientY,
+    });
+  };
   const handlerClick = ()=>{
-    setCount(count+step);
+    setState({
+      ...state,
+      count: state.count + 1
+    })
   }
-  const handlerInput = ({target:{value}})=>{
-    setStep(Number(value));
-  }
-  console.log('render')
   return (
-    <div>
-      <h2>Count: {count}</h2>
-      <button onClick={handlerClick}>add</button>
-      <input type="number" value={step} onChange={handlerInput}/>
+    <div
+      style={{ height: "80vh", backgroundColor: "#eee" }}
+      onMouseMove={handlerMove}
+      onClick={handlerClick}
+    >
+      <p>x: {state.x}</p>
+      <p>y: {state.y}</p>
+      <p>count: {state.count}</p>
     </div>
   );
-}
+};
 
 export default LearnHooks;
