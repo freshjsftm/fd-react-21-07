caff687e24622e34c56e1e1c12839cd52f7c05a4
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-08-17 14:54:40 +0300

    Example reexport & inlinestyle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useState} from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import FuncStopWatch from "./components/FuncStopWatch";
 import HomePage from "./pages/HomePage";
-import { UserContext } from "./contexts";
+import { UserContext, ThemeContext } from "./contexts";
 import UserCard from "./components/UserCard";
-
+import CONSTANTS from "./constants";
+const {THEMES} = CONSTANTS;
 const App = () => {
   const [user, setUser] = useState({
     id: 1,
@@ -13,21 +14,19 @@ const App = () => {
     lname: "Musk",
     isSelected: false,
   });
-  const [theme, setTheme] = useState(true);
-  const handlerTheme = ()=>{setTheme(!theme)}
+  const themeArrState = useState(THEMES.LIGHT);
   return (
-    <div style={{backgroundColor:(theme?'#fff':'#222'), color:(theme?'#222':'#fff')}}>
-      <UserContext.Provider value={[user, setUser]}>
-        <button onClick={handlerTheme}>switch theme</button>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/usercard" element={<UserCard />} />
-            <Route path="/func" element={<FuncStopWatch />} />
-          </Routes>
-        </BrowserRouter>
-      </UserContext.Provider>
-    </div>
+    <ThemeContext.Provider value={themeArrState}>
+        <UserContext.Provider value={[user, setUser]}>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/usercard" element={<UserCard />} />
+              <Route path="/func" element={<FuncStopWatch />} />
+            </Routes>
+          </BrowserRouter>
+        </UserContext.Provider>
+    </ThemeContext.Provider>
   );
 };
 export default App;
--- src/components/UserProfile/index.js
+++ src/components/UserProfile/index.js
@@ -1,13 +1,26 @@
 import React, {useContext} from 'react';
-import { UserContext } from '../../contexts';
+import { UserContext, ThemeContext } from '../../contexts';
+import CONSTANTS from "../../constants";
+import styles from './UserProfile.module.scss'
+const {THEMES} = CONSTANTS;
+
+const mapStyle={
+  [THEMES.LIGHT]:{backgroundColor:'#fff', color:'#222'},
+  [THEMES.DARK]:{backgroundColor:'#222', color:'#fff'}
+}
 
 const UserProfile = () => {
   const [{name}] = useContext(UserContext);
+  const [theme, setTheme] = useContext(ThemeContext);
+  const handlerTheme = () => {
+    setTheme(theme===THEMES.LIGHT?THEMES.DARK:THEMES.LIGHT);
+  };
   return (
-    <>
-      <h2>User profile</h2>
+    <section className={styles.container} style={mapStyle[theme]}>
+      <h2 className={styles.container_heading}>User profile</h2>
       <h3>Hi, {name}</h3>
-    </>
+      <button onClick={handlerTheme}>switch theme</button>
+    </section>
   );
 }
 
